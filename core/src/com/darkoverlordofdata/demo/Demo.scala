2f08f0ed08c1cc9127a2c82359c80fde7c09bd34
bruce <darkoverlordofdata@gmail.com>
2016-05-31 19:19:42 -0700

    init

---
+++ core/src/com/darkoverlordofdata/demo/Demo.scala
@@ -0,0 +1,51 @@
+package com.darkoverlordofdata.demo
+import com.badlogic.gdx.Game
+import com.uwsoft.editor.renderer.SceneLoader
+
+class Demo extends Game {
+
+  var menuScene: MenuScene = null
+  var gameScene: GameScene = null
+  var optionScene: MenuScene = null
+  var scoreScene: MenuScene = null
+
+  override def create(): Unit = {
+    menuGame()
+  }
+
+  def menuGame() {
+    val sceneLoader = new SceneLoader()
+    menuScene = new MenuScene(sceneLoader, new MenuUI(this, sceneLoader))
+    optionScene = null
+    scoreScene = null
+    gameScene = null
+    setScreen(menuScene)
+  }
+
+  def optionsGame() {
+    val sceneLoader = new SceneLoader()
+    menuScene = null
+    optionScene = new MenuScene(sceneLoader, new OptionUI(this, sceneLoader))
+    setScreen(optionScene)
+    scoreScene = null
+    gameScene = null
+  }
+
+  def scoreGame() {
+    val sceneLoader = new SceneLoader()
+    menuScene = null
+    optionScene = null
+    scoreScene = new MenuScene(sceneLoader, new ScoreUI(this, sceneLoader))
+    setScreen(scoreScene)
+    gameScene = null
+  }
+
+  def playGame() {
+    menuScene = null
+    optionScene = null
+    scoreScene = null
+    gameScene = new GameScene()
+    setScreen(gameScene)
+  }
+
+}
