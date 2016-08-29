9078a3d25ac347760bee865071f5ce6e6dc800cb
bruce davidson <darkoverlordofdata@gmail.com>
2016-08-28 21:43:50 -0700

    daily

--- android/src/com/darkoverlordofdata/entitas/demo/AndroidLauncher.java
+++ android/src/com/darkoverlordofdata/entitas/demo/AndroidLauncher.java
@@ -4,7 +4,7 @@ import android.os.Bundle;
 
 import com.badlogic.gdx.backends.android.AndroidApplication;
 import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
-import com.darkoverlordofdata.entitas.demo.GameScene;
+import com.darkoverlordofdata.demo.Demo;
 
 public class AndroidLauncher extends AndroidApplication {
 	@Override
